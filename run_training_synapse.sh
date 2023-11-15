8f79435aee8ca52db5b1f046a1a4db13294d251e
bravo-hq <yousef.sadegheih@ur.de>
2023-11-15 18:26:24 +0100

    updated the config

--- run_training_synapse.sh
+++ run_training_synapse.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=/cabinet/yousef/synapse/output_synapse_test_continuing
 export d_lka_former_preprocessed="$DATASET_PATH"/d_lka_former_raw/d_lka_former_raw_data/Task02_Synapse
 export d_lka_former_raw_data_base="$DATASET_PATH"/d_lka_former_raw
 
-python d_lka_former/run/run_training.py 3d_fullres d_lka_former_trainer_synapse 2 0 --continue_training --trans_block TransformerBlock  --depths 3 --skip_connections 4
+python d_lka_former/run/run_training.py 3d_fullres d_lka_former_trainer_synapse 2 0 --continue_training --trans_block TransformerBlock_3D_single_deform_LKA  --depths 3 --skip_connections 4
